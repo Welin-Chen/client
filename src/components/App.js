0b57244abb22071ef102f44a561fea1d3d8f9cf6
welin <a0929015591@gmail.com>
2022-05-18 22:14:28 +0800

    ready for deploy

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import history from "../history";
+
+// OAuth ID
+// 897364719390-c98ghavh3ccvpmslori5a4i2edlu99ce.apps.googleusercontent.com
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <Router history={history}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
